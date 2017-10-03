bf041d49be6b5ee16117b64fd8ba96399bb8f3e4
Bruno Parga <brunoparga@gmail.com>
2017-10-02 21:15:47 -0300

    Move castling and promotion to their own methods

--- lib/chess_class.rb
+++ lib/chess_class.rb
@@ -77,7 +77,7 @@ class Chess
   end
 
   def prompt(possible)
-    # For the time being, this assumes the player enter valid, existing squares.
+    # For the time being, this assumes the player enters valid, existing squares.
     # The argument is a hash of possible_moves.
     from, target = nil
     loop do
@@ -88,7 +88,7 @@ class Chess
       else
         puts "That is not a valid start for a move."
       end
-    end    # add more validation?
+    end
     loop do
       print "Target square: "
       target = gets.chomp.to_sym
@@ -104,37 +104,14 @@ class Chess
   def effect_move(from, target)
     # Realizes the requested move. Assumes it is valid.
     if is_castle(from, target)
-      # castling moves
-      rank = (@whites_turn ? 1 : 8)
-      @board[from].position = target
-      @board[target] = @board[from]
-      @board[from] = ' '
-      rook_start = (target == :"g#{rank}" ? :"h#{rank}" : :"a#{rank}")
-      rook_end = (target == :"g#{rank}" ? :"f#{rank}" : :"d#{rank}")
-      @board[rook_start].position = rook_end
-      @board[rook_end] = @board[rook_start]
-      @board[rook_start] = ' '
+      castle(from, target)
     elsif is_promotion(from, target)
-      puts "Which piece do you want to promote your pawn to?"
-      print "Choose one of (Q)ueen, (R)ook, (B)ishop, k(N)ight: "
-      choice = gets.chomp.upcase
-      case choice
-      when 'R'
-        @board[target] = Rook.new((@whites_turn ? :white : :black), target)
-        @board[target].has_moved = true
-      when 'B'
-        @board[target] = Bishop.new((@whites_turn ? :white : :black), target)
-      when 'N'
-        @board[target] = Knight.new((@whites_turn ? :white : :black), target)
-      else
-        @board[target] = Queen.new((@whites_turn ? :white : :black), target)
-      end
-      @board[from] = ' '
+      promote(from, target)
     else
       @board[from].position = target
       @board[target] = @board[from]
-      @board[from] = ' '
     end
+    @board[from] = ' '
     if @board[target].is_a?(King) or @board[target].is_a?(Rook)
       @board[target].has_moved = true
     end
@@ -147,9 +124,42 @@ class Chess
     @board[from].is_a?(King))
   end
 
+  def castle(from, target)
+    rank = (@whites_turn ? 1 : 8)
+    @board[from].position = target
+    @board[target] = @board[from]
+    rook_start = (target == :"g#{rank}" ? :"h#{rank}" : :"a#{rank}")
+    rook_end = (target == :"g#{rank}" ? :"f#{rank}" : :"d#{rank}")
+    @board[rook_start].position = rook_end
+    @board[rook_end] = @board[rook_start]
+    @board[rook_start] = ' '
+  end
+
   def is_promotion(from, target)
     rank = (@whites_turn ? 8 : 1).to_s
     @board[from].is_a?(Pawn) and target[1] == rank
+  end
+
+  def promote(from, target)
+    puts "Which piece do you want to promote your pawn to?"
+    print "Choose one of (Q)ueen, (R)ook, (B)ishop, k(N)ight: "
+    choice = gets.chomp.upcase
+    case choice
+    when 'R'
+      @board[target] = Rook.new((@whites_turn ? :white : :black), target)
+      # The next line is ****EXTREMELY IMPORTANT****
+      # I repeat, it is ****EXTREMELY IMPORTANT****
+      # It might be the single most important line in all of the program
+      # It implements the 1972 FIDE decision that a rook obtained through
+      # promotion cannot castle. Thank you for saving chess, FIDE!
+      @board[target].has_moved = true
+    when 'B'
+      @board[target] = Bishop.new((@whites_turn ? :white : :black), target)
+    when 'K', 'N'
+      @board[target] = Knight.new((@whites_turn ? :white : :black), target)
+    else
+      @board[target] = Queen.new((@whites_turn ? :white : :black), target)
+    end
   end
 
 end
